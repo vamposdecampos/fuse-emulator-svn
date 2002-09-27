219c3e0101f0838237d776d9916a8aa3cbc059f5
pak21 <pak21@6c7d1b9a-d430-0410-8293-a2b96dacb39b>
2002-09-27 14:39:05 +0000

    Remember to wrap the zlib calls in rzx_read_snapshot in #ifdef HAVE_ZLIB_H wrappers; forward port from fuse's Release-0_5_0-branch.
    git-svn-id: https://fuse-emulator.svn.sourceforge.net/svnroot/fuse-emulator/trunk@697 6c7d1b9a-d430-0410-8293-a2b96dacb39b

--- libspectrum/rzx.c
+++ libspectrum/rzx.c
@@ -248,7 +248,7 @@ rzx_read_snapshot( const libspectrum_byte **ptr, const libspectrum_byte *end,
 
   /* For deflated snapshot data: */
   int compressed;
-  libspectrum_byte *gzsnap; size_t uncompressed_length = 0;
+  libspectrum_byte *gzsnap = NULL; size_t uncompressed_length = 0;
 
   if( end - (*ptr) < 16 ) {
     libspectrum_print_error( "rzx_read_snapshot: not enough data in buffer" );
@@ -273,6 +273,9 @@ rzx_read_snapshot( const libspectrum_byte **ptr, const libspectrum_byte *end,
 
   /* If compressed, uncompress the data */
   if( compressed ) {
+
+#ifdef HAVE_ZLIB_H
+
     error = libspectrum_zlib_inflate( (*ptr) + 8, blocklength - 17,
 				      &gzsnap, &uncompressed_length );
     if( error != LIBSPECTRUM_ERROR_NONE ) return error;
@@ -285,6 +288,15 @@ rzx_read_snapshot( const libspectrum_byte **ptr, const libspectrum_byte *end,
       return LIBSPECTRUM_ERROR_CORRUPT;
     }   
     snap_ptr = gzsnap;
+
+#else			/* #ifdef HAVE_ZLIB_H */
+
+    libspectrum_print_error(
+      "rzx_read_snapshot: zlib needed for decompression\n"
+    );
+    return LIBSPECTRUM_ERROR_UNKNOWN;
+
+#endif			/* #ifdef HAVE_ZLIB_H */
 
   } else {
 
