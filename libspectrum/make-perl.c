f27ef983fbb9b8096840071c86ba06d98b52749a
pak21 <pak21@6c7d1b9a-d430-0410-8293-a2b96dacb39b>
2003-07-17 13:54:49 +0000

    Generate generate.pl rather than editing it :-(
    git-svn-id: https://fuse-emulator.svn.sourceforge.net/svnroot/fuse-emulator/trunk@1489 6c7d1b9a-d430-0410-8293-a2b96dacb39b

--- libspectrum/make-perl.c
+++ libspectrum/make-perl.c
@@ -1,5 +1,5 @@
 /* make-sed.c: Generate a sed script to create the libspectrum_* typedefs
-   Copyright (c) 2002 Philip Kendall, Darren Salt
+   Copyright (c) 2002-2003 Philip Kendall, Darren Salt
 
    $Id$
 
@@ -97,86 +97,6 @@ int main(void)
 #endif				/* #ifdef HAVE_STDINT_H */
 
   printf( "CODE\n}\n" );
-
-  printf( "\nif( /LIBSPECTRUM_GLIB_REPLACEMENT/ ) {\n\n" );
-
-#ifdef HAVE_LIB_GLIB
-
-  printf( "  $_ = \"#include <glib.h>\";\n\n" );
-
-#else				/* #ifdef HAVE_LIB_GLIB */
-
-  printf( "  $_ = << \"CODE\";\n"
-"/* Glib emulation */\n"
-"typedef int gint;\n"
-"typedef unsigned int guint;\n"
-"typedef const void * gconstpointer;\n"
-"typedef void * gpointer;\n"
-"\n"
-"typedef struct _GSList GSList;\n"
-"\n"
-"struct _GSList {\n"
-"    gpointer data;\n"
-"    GSList *next;\n"
-"};\n"
-"\n"
-"typedef void		(*GFunc)		(gpointer	data,\n"
-"						 gpointer	user_data);\n"
-"\n"
-"typedef gint		(*GCompareFunc)		(gconstpointer	a,\n"
-"						 gconstpointer	b);\n"
-"\n"
-"\n"
-"GSList* g_slist_insert_sorted	(GSList		*list,\n"
-"				 gpointer	 data,\n"
-"				 GCompareFunc	 func);\n"
-"\n"
-"GSList* g_slist_append		(GSList		*list,\n"
-"				 gpointer	 data);\n"
-"\n"
-"GSList* g_slist_remove		(GSList		*list,\n"
-"				 gpointer	 data);\n"
-"\n"
-"void	g_slist_foreach		(GSList		*list,\n"
-"				 GFunc		 func,\n"
-"				 gpointer	 user_data);\n"
-"\n"
-"void	g_slist_free		(GSList		*list);\n"
-"\n"
-"GSList* g_slist_nth		(GSList		*list,\n"
-"				 guint		n);\n"
-"\n"
-"GSList* g_slist_find_custom	(GSList		*list,\n"
-"				 gpointer	data,\n"
-"				 GCompareFunc	func );\n"
-"\n"
-"gint	g_slist_position	(GSList		*list,\n"
-"				 GSList		*llink);\n"
-"CODE\n" );
-
-#endif				/* #ifdef HAVE_LIB_GLIB */
-
-  printf( "}\n" );
-
-  printf( "\nif( /LIBSPECTRUM_SNAP_ACCESSORS/ ) {\n\n"
-"  open( DATAFILE, \"< snap_accessors.txt\" ) or die \"Couldn't open `snap_accessors.txt': $!\";\n\n"
-"  $_ = '';\n"
-"  while( <DATAFILE> ) {\n\n"
-"    next if /^\\s*$/; next if /^\\s*#/;\n\n"
-"    my( $type, $name, $indexed ) = split;\n\n"
-"    if( $indexed ) {\n\n"
-"	print << \"CODE\";\n"
-"$type libspectrum_snap_$name( libspectrum_snap *snap, int idx );\n"
-"void libspectrum_snap_set_$name( libspectrum_snap *snap, int idx, $type $name );\n"
-"CODE\n\n"
-"    } else {\n\n"
-"	print << \"CODE\";\n"
-"$type libspectrum_snap_$name( libspectrum_snap *snap );\n"
-"void libspectrum_snap_set_$name( libspectrum_snap *snap, $type $name );\n"
-"CODE\n\n"
-"    }\n"
-"  }\n"
-"}\n" );
 
   return 0;
 }
