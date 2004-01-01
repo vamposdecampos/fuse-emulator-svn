e930b72e1465725b617ecb21d6df1d7b70f819d1
pak21 <pak21@6c7d1b9a-d430-0410-8293-a2b96dacb39b>
2004-01-01 23:06:20 +0000

    Mention the Win32 port under 'What's New?'.
    git-svn-id: https://fuse-emulator.svn.sourceforge.net/svnroot/fuse-emulator/trunk@1858 6c7d1b9a-d430-0410-8293-a2b96dacb39b

--- website/fuse.php
+++ website/fuse.php
@@ -147,6 +147,9 @@ END_SECTION
 #<!-- ======================================================= -->
 
 fuse_section (NULL, "What's new?", <<<END_SECTION
+  <h3>1 January 2004</h3>
+  <p>A alpha version of a port of Fuse to Win32 is now <a href="fuse-win32.zip">available</a>. See the <a href="http://sourceforge.net/forum/forum.php?forum_id=341379">announcement</a> for more details.</p>
+
   <h3>0.6.1</h3>
   <ul>
    <li>RZX competition mode.</li>
