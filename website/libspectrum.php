e39cc0a07b29ace451707caba4554391f09d3030
zubzero <zubzero@6c7d1b9a-d430-0410-8293-a2b96dacb39b>
2008-02-17 00:35:12 +0000

    NetBSD, FreeBSD and Mandriva (PLF) now have packages of Fuse 0.9.0 and libspectrum 0.4.0.
    git-svn-id: https://fuse-emulator.svn.sourceforge.net/svnroot/fuse-emulator/trunk@3519 6c7d1b9a-d430-0410-8293-a2b96dacb39b

--- website/libspectrum.php
+++ website/libspectrum.php
@@ -91,10 +91,10 @@ fuse_section ("Download", "Downloads", <<<END_SECTION
   <ul>
    <li><a href="http://www.youmustbejoking.demon.co.uk/progs.stable.html">Debian stable</a> (Debian 4.0, "Etch"), <a href="http://www.youmustbejoking.demon.co.uk/progs.testing.html">testing</a>, <a href="http://www.youmustbejoking.demon.co.uk/progs.unstable.html">unstable</a>, <a href="http://www.youmustbejoking.demon.co.uk/progs.oldstable.html">Debian 3.1 ("Sarge")</a> (all 0.2.2) or <a href="http://www.youmustbejoking.demon.co.uk/progs.woody.html">Debian 3.0 ("Woody")</a> (0.2.0.1) by Darren Salt.</li>
    <li><a href="https://admin.fedoraproject.org/pkgdb/packages/name/libspectrum">Fedora</a> (0.4.0)</li>
-   <li><a href="http://www.FreeBSD.org/cgi/cvsweb.cgi/ports/emulators/libspectrum/">FreeBSD</a> (0.3.0.1)</li>
+   <li><a href="http://www.FreeBSD.org/cgi/cvsweb.cgi/ports/emulators/libspectrum/">FreeBSD</a> (0.4.0)</li>
    <li><a href="http://packages.gentoo.org/package/app-emulation/libspectrum?full_cat">Gentoo</a> (0.2.2)</li>
-   <li><a href="http://www.mandriva.com/">Mandriva</a> 10.0 packages of 0.3.0.1 are available from the <a href="http://plf.zarb.org/">PLF</a>, or some <a href="http://www.speccy.org/sinclairmania/arch/emu/mandrake/">older packages</a> from Miguel Barrio Orsikowsky.</li>
-   <li><a href="http://www.netbsd.org/">NetBSD</a> has 0.3.0.1 available as <a href="http://cvsweb.netbsd.org/bsdweb.cgi/pkgsrc/emulators/libspectrum/">emulators/libspectrum</a> in <a href="http://www.pkgsrc.org/">pkgsrc</a>.</li>
+   <li><a href="http://www.mandriva.com/">Mandriva</a> packages of 0.4.0 are available from the <a href="http://plf.zarb.org/">PLF</a>, or some <a href="http://www.speccy.org/sinclairmania/arch/emu/mandrake/">older packages</a> from Miguel Barrio Orsikowsky.</li>
+   <li><a href="http://www.netbsd.org/">NetBSD</a> has 0.4.0 available as <a href="http://cvsweb.netbsd.org/bsdweb.cgi/pkgsrc/emulators/libspectrum/">emulators/libspectrum</a> in <a href="http://www.pkgsrc.org/">pkgsrc</a>.</li>
    <li><a href="http://www.openbsd.org/">OpenBSD</a> users can find a port of 0.2.2 in <a href="http://www.openbsd.org/cgi-bin/cvsweb/ports/devel/libspectrum/">ports/devel/libspectrum</a>.</li>
    <li><a href="http://en.docs.pld-linux.org/">PLD</a> users have a package of 0.2.1 available.</li>
    <li><a href="mailto:spec(at)webtech(dot)pl">Marek Januszewski</a> has made a <a href="libspectrum-0.2.0.1-i386-1spec.tgz">Slackware 9.1</a> (<a href="libspectrum-0.2.0.1-i386-1spec.tgz.md5">md5 sum</a>) package of 0.2.0.1.</li>
