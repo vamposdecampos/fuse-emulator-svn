45b2189c436fc2b30310e1a1fcc199210a64298c
pak21 <pak21@6c7d1b9a-d430-0410-8293-a2b96dacb39b>
2008-07-01 08:08:40 +0000

    Allow calloc function to be changed.
    git-svn-id: https://fuse-emulator.svn.sourceforge.net/svnroot/fuse-emulator/trunk@3709 6c7d1b9a-d430-0410-8293-a2b96dacb39b

--- libspectrum/memory.c
+++ libspectrum/memory.c
@@ -77,6 +77,7 @@ void
 libspectrum_mem_set_vtable( libspectrum_mem_vtable_t *table )
 {
   libspectrum_malloc_fn = table->malloc;
+  libspectrum_calloc_fn = table->calloc;
   libspectrum_realloc_fn = table->realloc;
   libspectrum_free_fn = table->free;
 
