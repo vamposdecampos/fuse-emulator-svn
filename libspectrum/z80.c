4dca0a410da8572b4b3dd34fc156bb514169348f
fredm <fredm@6c7d1b9a-d430-0410-8293-a2b96dacb39b>
2011-01-31 10:47:01 +0000

    Note info loss of SpecDrum info when writing sna or z80 format snapshots.
    git-svn-id: https://fuse-emulator.svn.sourceforge.net/svnroot/fuse-emulator/trunk@4285 6c7d1b9a-d430-0410-8293-a2b96dacb39b

--- libspectrum/z80.c
+++ libspectrum/z80.c
@@ -1167,6 +1167,10 @@ libspectrum_z80_write2( libspectrum_byte **buffer, size_t *length,
   if( libspectrum_snap_custom_rom( snap ) )
     *out_flags |= LIBSPECTRUM_FLAG_SNAPSHOT_MAJOR_INFO_LOSS;
 
+  /* .z80 format doesn't save the Specdrum state at all */
+  if( libspectrum_snap_specdrum_active( snap ) )
+    *out_flags |= LIBSPECTRUM_FLAG_SNAPSHOT_MAJOR_INFO_LOSS;
+
   error = write_header( buffer, &ptr, length, out_flags, snap );
   if( error != LIBSPECTRUM_ERROR_NONE ) return error;
 
