f67ff4bc58aa878f48ec68efb10719bf684ab63d
Alex Badea <vamposdecampos@gmail.com>
2012-08-11 17:32:01 +0300

    if1_fdc: add periph type and setting for enabling it
    Add PERIPH_TYPE_INTERFACE1_FDC.  Make it PRESENT_OPTIONAL.  Add
    interface1_fdc bool setting, default False.
    
    Signed-off-by: Alex Badea <vamposdecampos@gmail.com>

--- fuse/peripherals/if1_fdc.c
+++ fuse/peripherals/if1_fdc.c
@@ -24,8 +24,12 @@
 
 */
 #include <config.h>
+#include "if1_fdc.h"
 
+#include "compat.h"
 #include "module.h"
+#include "periph.h"
+#include "settings.h"
 
 #if 0
 #define dbg(fmt, args...) fprintf(stderr, "%s:%d: " fmt "\n", __func__, __LINE__, ## args)
@@ -45,6 +49,22 @@ if1_fdc_romcs( void )
   dbg( "called" );
 }
 
+void
+if1_fdc_activate( void )
+{
+  dbg( "called" );
+}
+
+static periph_port_t if1_fdc_ports[] = {
+  {0, 0, NULL, NULL,},
+};
+
+static periph_t if1_fdc_periph = {
+  .option = &settings_current.interface1_fdc,
+  .ports = if1_fdc_ports,
+  .activate = if1_fdc_activate,
+};
+
 static module_info_t if1_fdc_module = {
   .reset = if1_fdc_reset,
   .romcs = if1_fdc_romcs,
@@ -54,4 +74,5 @@ void
 if1_fdc_init( void )
 {
   module_register( &if1_fdc_module );
+  periph_register( PERIPH_TYPE_INTERFACE1_FDC, &if1_fdc_periph );
 }
