248618195494d772ad9d96517b92a615516f8fc8
fredm <fredm@82cbdd72-22ac-47d8-a7df-4a7c34dd7743>
2014-05-07 12:24:20 +0000

    Select glibtoolize where required.
    git-svn-id: svn://svn.code.sf.net/p/fuse-emulator/code/trunk@5082 82cbdd72-22ac-47d8-a7df-4a7c34dd7743

--- fuse-utils/autogen.sh
+++ fuse-utils/autogen.sh
@@ -27,7 +27,10 @@
 set -e
 
 aclocal -I m4
-libtoolize --automake
+# Mac OS X: Use glibtoolize instead
+(glibtoolize --version) < /dev/null > /dev/null 2>&1 && LIBTOOLIZE=glibtoolize |
+| LIBTOOLIZE=libtoolize
+$LIBTOOLIZE --automake
 autoheader
 automake --add-missing
 autoconf
