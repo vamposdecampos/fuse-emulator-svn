80a93d4e79fa7288e99a4b76bd86231c7f71be50
fredm <fredm@82cbdd72-22ac-47d8-a7df-4a7c34dd7743>
2016-03-25 09:24:48 +0000

    Fix getopt_long arguments.
    git-svn-id: svn://svn.code.sf.net/p/fuse-emulator/code/trunk@5365 82cbdd72-22ac-47d8-a7df-4a7c34dd7743

--- fuse-utils/tape2pulses.c
+++ fuse-utils/tape2pulses.c
@@ -58,7 +58,7 @@ main( int argc, char **argv )
     { 0, 0, 0, 0 }
   };
 
-  while( ( c = getopt_long( argc, argv, "r:hV", long_options, NULL ) ) != -1 ) {
+  while( ( c = getopt_long( argc, argv, "hV", long_options, NULL ) ) != -1 ) {
 
     switch( c ) {
 
