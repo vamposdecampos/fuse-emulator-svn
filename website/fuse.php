f26be4c0b055206c206ca3047713bfa82f1aa17e
fredm <fredm@82cbdd72-22ac-47d8-a7df-4a7c34dd7743>
2016-06-07 10:07:23 +0000

    Update links to Windows binaries of 1.2.0
    git-svn-id: svn://svn.code.sf.net/p/fuse-emulator/code/trunk@5600 82cbdd72-22ac-47d8-a7df-4a7c34dd7743

--- website/fuse.php
+++ website/fuse.php
@@ -104,7 +104,7 @@ fuse_section ("Download", "Downloads", <<<END_SECTION
   <p>A native port to OS X by <a href="mailto:fredm@spamcop.net">Fredrick Meunier</a> is available on its own SourceForge project <a href="http://fuse-for-macosx.sourceforge.net/">here</a>, as well as a Spotlight importer for Mac OS X 10.4 Tiger users. Alternatively, the original version of Fuse will compile on OS X 10.3 (Panther) or later.</p>
 
   <h3>Windows</h3>
-  <p>A port to Windows of 1.1.1 by Sergio Baldovi is available <a href="http://sourceforge.net/projects/fuse-emulator/files/fuse/1.1.1/fuse-1.1.1-win32-setup.exe/download">here</a>, and the utilities are available <a href="http://sourceforge.net/projects/fuse-emulator/files/fuse-utils/1.1.1/fuse-utils-1.1.1-win32.zip/download">here</a>.</p>
+  <p>A port to Windows of 1.2.0 by Sergio Baldovi is available <a href="http://sourceforge.net/projects/fuse-emulator/files/fuse/1.2.0/fuse-1.2.0-win32-setup.exe/download">here</a>, and the utilities are available <a href="http://sourceforge.net/projects/fuse-emulator/files/fuse-utils/1.2.0/fuse-utils-1.2.0-win32.zip/download">here</a>.</p>
 
   <h3>Android</h3>
   <p>BogDan Vatra has ported Fuse 1.1.1 to Android OS, which could run on smartphones, tablets and TVs. Sources are available from <a href="https://github.com/bog-dan-ro/spectacol">GitHub</a> and binaries from <a href="https://play.google.com/store/apps/details?id=eu.licentia.games.spectacol">Google Play</a>.</p>
